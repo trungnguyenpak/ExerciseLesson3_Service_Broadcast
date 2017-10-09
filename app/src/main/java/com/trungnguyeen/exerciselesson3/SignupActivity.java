9020d846e14813ec01503af4b7875d3eb0494a81
Trung Nguyeen <huutrung1407@gmail.com>
2017-10-09 23:27:06 +0700

    Email validation, SignUp OK

--- app/src/main/java/com/trungnguyeen/exerciselesson3/SignupActivity.java
+++ app/src/main/java/com/trungnguyeen/exerciselesson3/SignupActivity.java
@@ -1,13 +1,91 @@
 package com.trungnguyeen.exerciselesson3;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
+
+
+    private EditText edtUsername;
+    private EditText edtEmail;
+    private EditText edtPassword;
+    private EditText edtConfirmPassword;
+    private Button btnSignUp;
+
+
+    public static final String USERNAME = "USERNAME";
+    public static final String TAG = SignupActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+
+        getWidgets();
+        setEvents();
+    }
+
+    private void getWidgets() {
+        edtUsername = (EditText) findViewById(R.id.edt_username);
+        edtEmail = (EditText) findViewById(R.id.edt_email);
+        edtPassword = (EditText) findViewById(R.id.edt_password);
+        edtConfirmPassword = (EditText) findViewById(R.id.edt_confirmPassword);
+        btnSignUp = (Button) findViewById(R.id.btn_signup);
+    }
+
+    private void setEvents() {
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String username = edtUsername.getText().toString();
+                String email = edtEmail.getText().toString();
+                String password = edtPassword.getText().toString();
+                String confirmPassword = edtConfirmPassword.getText().toString();
+
+                if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(email) &&
+                        !TextUtils.isEmpty(password) && !TextUtils.isEmpty(confirmPassword)){
+
+                    //check validate Email
+                    if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email.trim()).matches()){
+                        Toast.makeText(SignupActivity.this, "Email is Invalid.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+
+                    if(!password.equals(confirmPassword)){
+                        Toast.makeText(SignupActivity.this, "Confirm password does not match.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    Intent intent = new Intent(SignupActivity.this, UserInfoActivity.class);
+                    intent.putExtra(USERNAME, username);
+                    startActivity(intent);
+                    Toast.makeText(SignupActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    Toast.makeText(SignupActivity.this, "Please complete all!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+
+
+    //Hidden keyboard when touch outside
+    public void hideSoftKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) this.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), 0);
     }
 }
